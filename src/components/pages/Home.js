f2564647784ee6e00ec08c25652bb871df5634fb
KRivera2394 <keanurivera2394@gmail.com>
2022-08-29 04:06:36 -0400

    FixedHomeError

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
       <h1 className='homeHeader'>Keanu Rivera</h1>
       <img className='profilePic' src={pp} alt='ProfilePicture'/>
       <p className='homeText tH'>
-      Hello! Welcome to my portfolio! My name is Keanu Rivera and I just might be your next Junior Full-Stack Developer!. I am a detail-oriented individual with knowledge across a range of applications and programming languages including HTML, CSS, JavaScript, MySQL, GraphSQL , and MongoDB. I'm always looking to utilize my programming skills to provide solutions to complex problems and create fully functional web applications for users to enjoy. 
+      Hello! Welcome to my portfolio! My name is Keanu Rivera and I just might be your next Junior Full-Stack Developer!. I am a detail-oriented individual with knowledge across a range of applications and programming languages including HTML, CSS, JavaScript, MySQL, GraphSQL , and MongoDB. I'm always looking to utilize my skills to provide solutions for complex problems and create fully functional web applications for users to enjoy! 
 
       
       </p>
