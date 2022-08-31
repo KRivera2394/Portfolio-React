96c3f26346018d66c28e3ad02ac543f6f6f3f114
KRivera2394 <keanurivera2394@gmail.com>
2022-08-31 03:06:38 -0400

    Updates

---
+++ precache-manifest.437ff85625205753c2423ef0756cd9cb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "940193deac60ab2ce1a4ddea0e4f3a03",
+    "url": "/Portfolio-React/index.html"
+  },
+  {
+    "revision": "3ddca2b2b591774acc13",
+    "url": "/Portfolio-React/static/css/2.a779dd76.chunk.css"
+  },
+  {
+    "revision": "2c34f56caa26b3ca9b45",
+    "url": "/Portfolio-React/static/css/main.2c232b50.chunk.css"
+  },
+  {
+    "revision": "3ddca2b2b591774acc13",
+    "url": "/Portfolio-React/static/js/2.7dac88c0.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/Portfolio-React/static/js/2.7dac88c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c34f56caa26b3ca9b45",
+    "url": "/Portfolio-React/static/js/main.50d819c1.chunk.js"
+  },
+  {
+    "revision": "80390daac13b253e12be",
+    "url": "/Portfolio-React/static/js/runtime-main.88f10ecf.js"
+  },
+  {
+    "revision": "71c361de92ba160bfa21108fe98686c3",
+    "url": "/Portfolio-React/static/media/HeadShot-BangJJ.71c361de.jpg"
+  },
+  {
+    "revision": "77d353e6ed1d7a91c29af0675f90544d",
+    "url": "/Portfolio-React/static/media/WeatherBot.77d353e6.png"
+  },
+  {
+    "revision": "a0e114d870a1cea9f25fa60184bee477",
+    "url": "/Portfolio-React/static/media/cluster.a0e114d8.png"
+  },
+  {
+    "revision": "5785f274965447848549800e53930f4b",
+    "url": "/Portfolio-React/static/media/screenshot.5785f274.png"
+  },
+  {
+    "revision": "1434fb0105a6a16b9c1e4177a39b6b0c",
+    "url": "/Portfolio-React/static/media/underConstruction.1434fb01.jpg"
+  }
+]);
