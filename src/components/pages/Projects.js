5fa56a99f1e62bf1d0d7c44d40c62ad1ef3f2d25
KRivera2394 <keanurivera2394@gmail.com>
2022-09-07 11:45:36 -0400

    Added Elevate

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import vNV from '../images/screenshot.png'
 import cluster from '../images/cluster.png'
-import weatherBot from '../images/WeatherBot.png'
+// import weatherBot from '../images/WeatherBot.png'
+import elevate from '../images/elevate.png'
 import '../styles/projects.css';
 import { motion } from 'framer-motion';
 
@@ -21,13 +22,15 @@ export default function Projects() {
       <div className='projectCont'>
       <motion.a initial={{ x: -1400}}
       animate={{x: 0}}
-      transition={{ delay: 1.0 , type: 'tween'}} className='vnvImage' href='https://krivera2394.github.io/Vines-and-Victuals/'><img className='vIG' src={vNV} alt='vnv'/></motion.a>
+      transition={{ delay: 1.0 , type: 'tween'}} className='vnvImage' href='https://elevate-2.herokuapp.com/'><img className='vIG' src={elevate} alt='WeatherBot'/></motion.a>
+     
        <motion.a initial={{ x: 1400}}
       animate={{x: 0}}
       transition={{ delay: 1.0 , type: 'tween'}} className='vnvImage' href='https://clusterr-1.herokuapp.com/home'><img className='vIG' src={cluster} alt='cluster'/></motion.a>
+     
        <motion.a initial={{ x: -1400}}
       animate={{x: 0}}
-      transition={{ delay: 1.0 , type: 'tween'}} className='vnvImage' href='https://krivera2394.github.io/5-Day-Forecast/'><img className='vIG' src={weatherBot} alt='WeatherBot'/></motion.a>
+      transition={{ delay: 1.0 , type: 'tween'}} className='vnvImage' href='https://krivera2394.github.io/Vines-and-Victuals/'><img className='vIG' src={vNV} alt='vnv'/></motion.a>
       </div>
     </div>
   );
